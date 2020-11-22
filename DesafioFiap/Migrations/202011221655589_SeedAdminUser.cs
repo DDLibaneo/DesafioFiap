cc024ab3f54c12fe58cc27937be9566e2915ea7a
Daniel Davi Libâneo <daniel.libaneo@gmail.com>
2020-11-22 14:10:34 -0300

    add migration to seed admin user

---
+++ DesafioFiap/Migrations/202011221655589_SeedAdminUser.cs
@@ -0,0 +1,21 @@
+﻿namespace DesafioFiap.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SeedAdminUser : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"
+                INSERT [dbo].[AspNetUsers] ([Id], [Email], [EmailConfirmed], [PasswordHash], [SecurityStamp], [PhoneNumber], [PhoneNumberConfirmed], [TwoFactorEnabled], [LockoutEndDateUtc], [LockoutEnabled], [AccessFailedCount], [UserName]) 
+                VALUES (N'40366ecb-3776-4618-b7a1-74d63d7b3d06', N'admin@gmail.com', 0, N'ADQHRXjqJOnhQ2WX7bnsFaSZ2o1bowb2M8j9ia9sQweDMFEHGjyco4IUiNGtU4e+5g==', N'fe552141-b66d-47ac-80b4-b8306752b4d5', NULL, 0, 0, NULL, 1, 0, N'admin@gmail.com');
+                "
+            );
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
